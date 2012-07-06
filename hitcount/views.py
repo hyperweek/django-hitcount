79c85829b38ab9bdb274edd9527fb780c3a77c09
zyegfryed <zyegfryed@gmail.com>
2012-07-06 11:07:56 +0200

    Use the require_POST decorator

--- hitcount/views.py
+++ hitcount/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404, HttpResponse, HttpResponseBadRequest
 from django.utils import simplejson
 from django.conf import settings
+from django.views.decorators.http import require_POST
 
 from hitcount.utils import get_ip
 from hitcount.models import Hit, HitCount, BlacklistIP, BlacklistUserAgent
@@ -77,6 +78,7 @@ def json_error_response(error_message):
 # right now the django handling isn't great.  should return the current
 # hit count so we could update it via javascript (since each view will
 # be one behind).
+@require_POST
 def update_hit_count_ajax(request):
     '''
     Ajax call that can be used to update a hit count.
@@ -90,9 +92,6 @@ def update_hit_count_ajax(request):
     # make sure this is an ajax request
     if not request.is_ajax():
         raise Http404()
-
-    if request.method == "GET":
-        return json_error_response("Hits counted via POST only.")
 
     hitcount_pk = request.POST.get('hitcount_pk')
 
